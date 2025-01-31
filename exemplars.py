8d0d36d9a3385e9e8922d656b8d81f0ddc0c6ba5
Chris Simpkins <chris@sourcefoundry.org>
2025-01-31 18:00:58 -0500

    define gzip mtime=0 to eliminate time-dependency in the gzip data
    supports deterministic JSON output across commits

--- exemplars.py
+++ exemplars.py
@@ -266,7 +266,12 @@ def write_json_files(data: Dict[str, Any], output_dir: str) -> None:
         json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
     with (json_dir / "icu_exemplars-min.json").open("w", encoding="utf-8") as f:
         f.write(minified_data)
-    with gzip.open(json_dir / "icu_exemplars-min.json.gz", "wb", compresslevel=9) as f:
+    with gzip.GzipFile(
+        filename=str(json_dir / "icu_exemplars-min.json.gz"),
+        mode="wb",
+        compresslevel=9,
+        mtime=0,
+    ) as f:
         f.write(minified_data.encode("utf-8"))
 
 
